e01f1978abbf7654112eea070514defe4b8fd15c
IsStephy <145896071+IsStephy@users.noreply.github.com>
2025-02-22 13:38:18 +0200

    adapt changing color depending who speak

--- panel_container.gd
+++ panel_container.gd
@@ -15,7 +15,11 @@ func _process(delta: float) -> void:
 
 func _changePanelColor() -> void:
 	var dialogueBox = self
+	var poligon = $Polygon2D
+	var label =$Polygon2D/Label
 	if currentSpeaking == BOBIK:
-		dialogueBox.modulate = Color(0, 0, 0)
+		dialogueBox.modulate = Color(0.288, 0.091, 0.668)
+		poligon.modulate =Color(0.288, 0.091, 0.7)
+		label.modulate = Color(1,1,1)
 	else:
 		dialogueBox.modulate = Color(1, 1, 1)
