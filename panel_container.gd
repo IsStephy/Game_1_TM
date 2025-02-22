912a2a81664f867016404d42f3abee2378e4b879
Dackohn <145903356+Dackohn@users.noreply.github.com>
2025-02-22 17:01:20 +0200

    Some perfecting changes

--- panel_container.gd
+++ panel_container.gd
@@ -12,10 +12,15 @@ func _notification(what):
 		_resize_panel()
 
 func _changePanelColor() -> void:
+	var dialogueBox = self
+	var poligon = $Polygon2D
+	var label =$Polygon2D/Label
 	if currentSpeaking == BOBIK:
-		modulate = Color(0, 0, 0)
+		dialogueBox.modulate = Color(0.288, 0.091, 0.668)
+		poligon.modulate =Color(0.288, 0.091, 0.7)
+		label.modulate = Color(10,10,10)
 	else:
-		modulate = Color(1, 1, 1)
+		dialogueBox.modulate = Color(1, 1, 1)
 
 func _resize_panel() -> void:
 	var parent_node = get_parent()
