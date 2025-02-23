2ce0f043c46bcc037f58f02ae0ead4348740c4c8
IsStephy <145896071+IsStephy@users.noreply.github.com>
2025-02-23 16:55:56 +0200

    Main menu add buttons

--- canvas_layer.gd
+++ canvas_layer.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 @onready var label = $dead_screen/Label
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.size = Vector2(800, 600)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
