0388586d835ca601910058a43650e066dce1ef2a
Dackohn <145903356+Dackohn@users.noreply.github.com>
2025-02-23 16:08:38 +0200

    Tried to resize

--- canvas_layer.gd
+++ canvas_layer.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 @onready var label = $dead_screen/Label
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	self.size = Vector2(800, 600)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
