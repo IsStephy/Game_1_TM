cb01d2b49f5e3ba07b986feb30d0c88e3272f282
Dackohn <145903356+Dackohn@users.noreply.github.com>
2025-02-24 08:11:10 +0200

    Made the menu to dissapear when starting the game

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,53 +1,71 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 @onready var exit_but = $Exit_Button
 @onready var continue_but = $Continue_Button
 @onready var start_but = $Start_Button
 @onready var save_but = $Save_Button
 @onready var load_but = $Load_Button
 @onready var color = $ColorRect
-var is_menu = false
-func _input(event):
+var is_menu
+
+var input_locked = false
+
+func _ready() -> void:
+	if self.get_parent() == $Window:
+		is_menu = true
+	else:
+		is_menu = false
+	update_menu_visibility()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if input_locked:
+		return
+
 	if event.is_action_pressed("ui_cancel"):
 		is_menu = !is_menu
 	update_menu_visibility()
 
 func update_menu_visibility():
-  # Show/hide the menu buttons
-	exit_but.visible = is_menu
-	continue_but.visible = is_menu
-	start_but.visible = is_menu
-	save_but.visible = is_menu
-	load_but.visible = is_menu
-	color.visible = is_menu
+	print("update_menu_visibility: is_menu =", is_menu)  # Debug output.
 	if is_menu:
+		exit_but.show()
+		continue_but.show()
+		start_but.show()
+		save_but.show()
+		load_but.show()
+		color.show()
 		self.mouse_filter = Control.MOUSE_FILTER_STOP
 	else:
+		exit_but.hide()
+		continue_but.hide()
+		start_but.hide()
+		save_but.hide()
+		load_but.hide()
+		color.hide()
 		self.mouse_filter = Control.MOUSE_FILTER_IGNORE
 
-func _on_continue_button_pressed() -> void:
-	is_menu = !is_menu
 
+func _on_continue_button_pressed() -> void:
+	is_menu = false
+	update_menu_visibility()
 
 func _on_start_button_pressed() -> void:
+	is_menu = false
+	update_menu_visibility()
 	var dialoguebox = $"../TextField/DialogueBox"
 	if dialoguebox:
 		dialoguebox.start("1")
-
+	await(get_tree().create_timer(10))
 	get_tree().call_deferred("change_scene_to_file", "res://scenes/background1.tscn")
 
 
-func _on_load_button_pressed() -> void:
-	pass # Replace with function body.
 
+
+func _on_load_button_pressed() -> void:
+	pass 
 
 func _on_save_button_pressed() -> void:
-	pass # Replace with function body.
-
+	pass 
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
