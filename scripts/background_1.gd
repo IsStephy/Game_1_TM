f5f87fbe132170acd8a77fd676a5f0352be3b3d2
Dackohn <145903356+Dackohn@users.noreply.github.com>
2025-02-25 19:40:09 +0200

    Added load square. Fixed the load.

--- scripts/background_1.gd
+++ scripts/background_1.gd
@@ -1,20 +1,49 @@
 extends Node2D
 
-
 @onready var label = $CanvasLayer/dead_screen/Label
 @onready var dialogue_box = $CanvasLayer/TextField/DialogueBox
 @onready var color = $CanvasLayer/dead_screen/ColorRect
-# Called when the node enters the scene tree for the first time.
+@onready var portrait = $CanvasLayer/TextField/PortraitVBox
+@onready var menu = $CanvasLayer/Main_Menu
+var is_load_screen 
+var game_started = false
+
 func _ready() -> void:
+	menu.is_menu = true
+	portrait.hide()
+	dialogue_box.hide()
+	dialogue_box.mouse_filter = Control.MOUSE_FILTER_IGNORE 
 	label.mouse_filter = Control.MOUSE_FILTER_IGNORE
 	color.mouse_filter = Control.MOUSE_FILTER_IGNORE
 	label.visible = false
 	color.visible = false
 
-	
-	dialogue_box.start('1')
+	print("Game loaded. Waiting for player to start.")
 
+func start_game():
+	print("Game started!")
+	game_started = true
+	dialogue_box.show()
+	dialogue_box.mouse_filter = Control.MOUSE_FILTER_STOP
+	dialogue_box.start('1') 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func load_game():
+	if FileAccess.file_exists("user://savegame.json"):
+		var file = FileAccess.open("user://savegame.json", FileAccess.READ)
+		var data_string = file.get_as_text()
+		file.close()
+
+		var json = JSON.new()
+		var error = json.parse(data_string)
+		var save_data = json.data
+
+		if typeof(save_data) == TYPE_DICTIONARY:
+			print("Game loaded!")
+			game_started = true
+			dialogue_box.show()
+			dialogue_box.mouse_filter = Control.MOUSE_FILTER_STOP
+			dialogue_box.get_dialogue_parser().load_save_data(save_data)
+		else:
+			print("Error: Save file is corrupted.")
+	else:
+		print("No save file found.")
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -6,20 +6,24 @@ extends Control
 @onready var save_but = $Save_Button
 @onready var load_but = $Load_Button
 @onready var color = $ColorRect
-var is_menu
+@onready var dialogue_box = $"../TextField/DialogueBox"
+@onready var texture = $"../Sprite2D"
+var is_menu = true
+var game_started = false 
+var is_load_screen = false
 
 var input_locked = false
 
 func _ready() -> void:
+	is_menu = true  
 	update_menu_visibility()
-
 
 func _unhandled_input(event: InputEvent) -> void:
 	if input_locked:
 		return
-	if event.is_action_pressed("ui_cancel"):
+	if event.is_action_pressed("ui_cancel") and game_started:
 		is_menu = !is_menu
-	update_menu_visibility()
+		update_menu_visibility()
 
 func update_menu_visibility():
 	if is_menu:
@@ -39,53 +43,55 @@ func update_menu_visibility():
 		color.hide()
 		self.mouse_filter = Control.MOUSE_FILTER_IGNORE
 
-
 func _on_continue_button_pressed() -> void:
-	is_menu = false
-	update_menu_visibility()
+	if game_started:
+		is_menu = false
+		update_menu_visibility()
 
 func _on_start_button_pressed() -> void:
 	is_menu = false
+	game_started = true 
 	update_menu_visibility()
-	var dialoguebox = $"../TextField/DialogueBox"
-	if dialoguebox:
-		dialoguebox.start("1")
-	get_tree().call_deferred("change_scene_to_file", "res://scenes/background1.tscn")
 
-
-
+	if get_tree().current_scene.name == "MainMenu":
+		get_tree().change_scene_to_file("res://scenes/background1.tscn")
+		await get_tree().process_frame 
+	var game_scene = get_tree().current_scene
+	if game_scene.has_method("start_game"):
+		game_scene.start_game()
+	else:
+		print("Error: Game scene does not have start_game() function!")
 
 func _on_load_button_pressed() -> void:
 	is_menu = false
+	game_started = true
+	update_menu_visibility()
+
 	if FileAccess.file_exists("user://savegame.json"):
 		var file = FileAccess.open("user://savegame.json", FileAccess.READ)
 		var data_string = file.get_as_text()
 		file.close()
+
 		var json = JSON.new()
 		var error = json.parse(data_string)
 		var save_data = json.data
-		if typeof(save_data) == TYPE_DICTIONARY:
-			var dialogue_box = get_node("../TextField/DialogueBox")
-			var dialogue_parser = dialogue_box._dialogue_parser
-			dialogue_parser.load_save_data(save_data)
-			print("Game loaded!")
+
+		await get_tree().process_frame 
+		var game_scene = get_tree().current_scene
+		if dialogue_box.has_method("load_game") and game_started:
+			dialogue_box.load_game(save_data)
 		else:
-			print("Error: Save file is corrupted.")
-	else:
-		print("No save file found.")
-
-
+			print("Error: Game scene does not have load_game() function!")
 
 
 
 func _on_save_button_pressed() -> void:
 	is_menu = false
-	var dialogue_box = get_node("../TextField/DialogueBox") 
 	var dialogue_parser = dialogue_box._dialogue_parser
 	var save_data = dialogue_parser.get_save_data()
 
 	var file = FileAccess.open("user://savegame.json", FileAccess.WRITE)
-	if file:
+	if file and game_started:
 		var json = JSON.new()
 		var json_string = json.stringify(save_data)
 		file.store_string(json_string)
@@ -93,8 +99,7 @@ func _on_save_button_pressed() -> void:
 		print("Game saved!")
 	else:
 		print("Error: Could not open save file for writing.")
-
-
+		
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
